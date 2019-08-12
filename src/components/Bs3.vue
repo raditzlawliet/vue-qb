dd855adebf65e37adf0d44452f73f62c9be3525a
Radityo Hernanda <radityohernanda@gmail.com>
2019-08-12 14:19:15 +0700

    vue-qb-bs3: separate build for bs3 theme extension (usage with template="bs3")

---
+++ src/components/Bs3.vue
@@ -0,0 +1,3 @@
+<style lang="scss">
+@import "@/assets/sass/bs3vqb/bs3vqb.scss";
+</style>
---
+++ src/components/index-bs3.js
@@ -0,0 +1,42 @@
+import Bs3 from "./Bs3.vue";
+// import Vue from "vue";
+// import FlockBanner from "./Banner.vue";
+
+// import Selector from "./Selector.vue";
+// import DynamicSelector from "@/components/DynamicSelector.vue";
+
+// import FTrim from "./functions/Trim.vue";
+// import FValue from "./functions/Value.vue";
+// import FCase from "./functions/Case.vue";
+
+// import globalFunctions from "@/components/globalFunctions.js";
+// import globalSettings from "@/components/globalSettings.js";
+
+// const Components = {
+// FlockBanner,
+// Selector,
+// DynamicSelector,
+
+// FTrim, 
+// FValue, 
+// FCase
+// };
+
+// Object.keys(Components).forEach(name => {
+//     Vue.component(name, Components[name]);
+// });
+
+// Object.keys(globalFunctions).forEach(name => {
+//     if (name && globalFunctions[name].component && !globalFunctions[name].isCustom) {
+//         // console.log('Register Component', "F" + name)
+//         Vue.component("F" + name, globalFunctions[name].component);
+//     }
+// });
+
+export default {
+    // Components,
+    // globalFunctions,
+    // globalSettings,
+    Bs3
+};
+
