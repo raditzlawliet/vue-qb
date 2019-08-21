2c925949a2851f5dccb9fb4f30656dc32db75aad
Radityo Hernanda <radityohernanda@gmail.com>
2019-08-21 22:22:43 +0700

    add stores add utilities

---
+++ src/stores/stores.js
@@ -0,0 +1,22 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        totalTvCount: 10 // The TV inventory
+    },
+
+    getters: {
+        // Here we will create a getter
+    },
+
+    mutations: {
+        // Here we will create Jenny
+    },
+
+    actions: {
+        // Here we will create Larry
+    }
+});
